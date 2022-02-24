25559bff99c6d138a9a7c2d21bf37b08227d6c9c
giabuynh <giabinh0899@gmail.com>
2022-02-24 10:45:07 +0700

    fix read-receipts

--- src/components/MessageForms.jsx
+++ src/components/MessageForms.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
+import { PictureOutlined, SendOutlined } from "@ant-design/icons";
 
 const MessageForms = (props) => {  
   const [value, setValue] = useState('');
@@ -8,11 +9,17 @@ const MessageForms = (props) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const text = value.trim();
+    if (text.length > 0) sendMessage(creds, chatId, { text });
+    setValue('');
   }
 
   const handleChange = (event) => {
     setValue(event.target.value);
     isTyping(props, chatId);
+  }
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.value, text: "" })
   }
 
   return(
@@ -24,6 +31,21 @@ const MessageForms = (props) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input 
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={ handleUpload }
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
     </form>
   );
 }
